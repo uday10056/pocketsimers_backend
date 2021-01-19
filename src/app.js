57c2d0ca5e869e4c7376720078b3632751181c27
uday10056 <53361926+uday10056@users.noreply.github.com>
2021-01-19 22:34:30 +0530

    Organized app.js and addded skin model validation

--- src/app.js
+++ src/app.js
@@ -1,74 +1,60 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-// imported db conn
+// --------------- Import Starts-------------
+// MongoDb Connection
 require("./db/conn");
-// imported skins model
+//External Libraries
+const express = require("express");
+//  Models
 const Skins = require("./db/models/skins");
+// --------------- Import Ends-------------
+
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 2070;
-app.use(bodyParser.json());
 
-// adding new skin
-app.post("/add",(req,res)=>{
-    console.log(req.body);
-    const skin = new Skins(req.body);
-    console.log(skin);
-    skin.save().then(()=>{
-        res.status(201).send(skin);
-    }).catch((e)=>{
-        res.status(400).send(e);
-    })
+// -------------  Adding New Skins -------------
+app.post("/add", async (req, res) => {
+    try {
+        const skin = new Skins(req.body);
+        await skin.save()
+        return res.status(201).send(skin);
+    }
+    catch (e) {
+        return res.status(400).send(e);
+    }
 });
 
-// getting individual skins data
-app.get("/skin/:id", async(req,res)=>{
-    try{
-        const _id = req.params.id;
-        console.log(_id);
-        const singleSkinData = await Skins.findById(_id);
-        if(!singleSkinData){
+// ------------- Fetching Individual Skin ---------
+app.get("/skin/:id", async (req, res) => {
+    try {
+        const singleSkinData = await Skins.findById(req.params.id);
+        if (!singleSkinData) {
             return res.status(404).send();
-        }else{
-            console.log(singleSkinData);
-            res.send(singleSkinData);
+        } else {
+            return res.send(singleSkinData);
         }
-    }catch(e){
-        res.send(e);
+    } catch (e) {
+        return res.status(400).send(e);
     }
 });
 
-// getting all skins data
-app.get("/allskins", async(req,res)=>{
-    try{
-        const allSkinsData = await Skins.find();
-        res.send(allSkinsData) 
-    }catch(e){
-        res.send(e);
+// ----------------- Searching Skins -------------
+app.post("/search", async (req, res) => {
+    try {
+        if (!req.body.title && !req.body.gameName) {
+            return res.status(400).send("title and gameName is Required");
+        }
+        const regex = new RegExp(escapeRegex(req.body.title), 'gi');
+        let searchQuery = null;
+        searchQuery = req.body.gameName == "all" ? { "title": regex } : { gameName: req.body.gameName, "title": regex };
+        const found = await Skins.find(searchQuery);
+        if (found.length == 0) {
+            return res.status(404).send();
+        } else {
+            return res.send(found);
+        }
     }
-});
-
-// searching skin
-app.post("/search", (req, res) =>{
-    console.log(req.body.title);
-    if (req.body.title && req.body.gameName) {
-       const regex = new RegExp(escapeRegex(req.body.title), 'gi');
-       let searchQuery = null;
-       if(req.body.gameName == "all"){
-           searchQuery = {"title": regex }
-       }
-       else{
-           searchQuery = {gameName:req.body.gameName, "title": regex }
-       }
-       Skins.find(searchQuery, (err, found)=> {
-           if(err) {
-               console.log(err);
-           } else {
-              res.send({op:found});
-           }
-       }); 
-    }
-    else{
-        res.status(400).send("title and gameName is Required");
+    catch (e) {
+        return res.status(400).send(e);
     }
 });
 
@@ -78,7 +64,7 @@ function escapeRegex(text) {
 
 
 
-// server listing at port
-app.listen(port,()=>{
+// Server listing at port
+app.listen(port, () => {
     console.log(`Server running on port ${port}...`)
 })
--- src/db/models/skins.js
+++ src/db/models/skins.js
@@ -1,42 +1,49 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const skinsSchema = new mongoose.Schema({
 
     gameName: {
-        type:String,
-        required:true,
+        type: String,
+        required: true,
     },
-    title : {
-        type:String,
-        required:true,
+    title: {
+        type: String,
+        required: true,
     },
-    tags : {
-        type:String,
-        required:true,
+    tags: {
+        type: String,
+        required: true,
     },
-    gameLink : {
-        type:String,
-        required:true,
+    gameLink: {
+        type: String,
+        required: true,
+        validate: (value) => {
+            return validator.isURL(value);
+        }
     },
-    publishedBy : {
-        type:String,
-        required:true,
+    publishedBy: {
+        type: String,
+        required: true,
     },
-    downloads : {
-        type:Number,
-        required:true,
-        default:0,
+    downloads: {
+        type: Number,
+        required: true,
+        default: 0,
     },
-    uploadTime : {
-        type:String,
-        required:true,
+    uploadTime: {
+        type: Date,
+        default: Date.now,
     },
-    imageUrl : {
-        type:String,
-        required:true,
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: (value) => {
+            return validator.isURL(value);
+        }
     },
 })
 
-//creating collection
+//Creating collection
 const Skins = new mongoose.model('Skins', skinsSchema);
 module.exports = Skins;
